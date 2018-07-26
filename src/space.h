acaf38eb9a9acd0bb102fc4a42214d7243b20506
lw <me@lw.ag>
2018-07-26 16:23:09 +0200

    Add remove_array_item.

--- src/space.h
+++ src/space.h
@@ -1,6 +1,9 @@
 #ifndef VT_H
 #define VT_H
 
+#include <stddef.h>
+#include <string.h>
+
 extern int term_w;
 extern int term_h;
 
@@ -48,6 +51,17 @@ void clamp_in_terminal(position *p);
 void get_terminal_dimensions(int *columns, int *lines);
 void restore_terminal();
 void set_terminal_nonblock();
+
+static inline int remove_array_item(void *array, int index, int length,
+	size_t item_size)
+{
+	size_t byte_offset = item_size * index;
+	size_t new_size = item_size * (length - index - 1);
+
+	memmove(array + byte_offset, array + byte_offset + item_size, new_size);
+
+	return length - 1;
+};
 
 
 #endif /* VT_H */
